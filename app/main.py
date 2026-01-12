c836d6778027d7af96ba1f3c07b2c20cb0ffe7a5
Venkatesh Kamat <venkatesh.kamat07@gmail.com>
2026-01-12 12:05:34 -0600

    Fix tests and deprecated pydantic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from app.api.router import api_router
 from app.db.base import Base
 from app.db.session import engine
@@ -7,10 +9,12 @@ from app.db.models import URL
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()
+    yield
+
+
 app = FastAPI(title="URL Shortner")
 
 app.include_router(api_router)
-
-@app.on_event("startup")
-def on_startup():
-    create_tables()
