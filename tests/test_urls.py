dba659ce7993ab6990af2b3cf1587d16d994cf8e
Venkatesh Kamat <venkatesh.kamat07@gmail.com>
2026-01-25 23:44:27 -0600

    Add tests for stat endpoint

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -25,3 +25,22 @@ def test_shorten_short_code_format(client):
     short_code = res.json()["short_code"]
     assert len(short_code) == 6
     assert short_code.isalnum()
+
+def test_stats_success(client):
+    create  = client.post('/shorten', json={"original_url":"https://example.com/page"})
+    assert create.status_code == 200
+    short_code = create.json()['short_code']
+
+    res = client.get(f'/stats/{short_code}')
+    assert res.status_code == 200
+    data = res.json()
+    assert data["original_url"] == "https://example.com/page"
+    assert data["short_code"] == short_code
+    assert data["clicks"] == 0
+    assert "created_at" in data
+    assert "id" in data
+
+def test_stats_not_found(client):
+    res = client.get("/stats/nonexistent")
+    assert res.status_code == 404
+    assert "URL not found" in res.json()["detail"]
